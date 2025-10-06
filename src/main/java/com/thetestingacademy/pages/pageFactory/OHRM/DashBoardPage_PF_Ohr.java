bfad1590d4d2a6fd0b0fc0aa180badf64b82ba58
monuRohtash <rohtashkumar.mk87@gmail.com>
2025-10-06 20:09:55 +0530

    TESTNG FILE  smoke mmmmmmdev

--- src/main/java/com/thetestingacademy/pages/pageFactory/OHRM/DashBoardPage_PF_Ohr.java
+++ src/main/java/com/thetestingacademy/pages/pageFactory/OHRM/DashBoardPage_PF_Ohr.java
@@ -30,7 +30,11 @@ public class DashBoardPage_PF_Ohr extends CommonToAllPage {
 
 //     // ✅ Verify Dashboard is Visible
     public String dislpayDashboard() {
-
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return getText(dashboardHeader);
     }
 
